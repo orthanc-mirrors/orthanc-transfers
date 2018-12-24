035d24c7b2b5be6d8e63e654a7eaa61cee773bf1
Sebastien Jodogne <s.jodogne@gmail.com>
2018-12-24 13:45:31 +0100

    preparing for 2019

--- Framework/TransferQuery.cpp
+++ Framework/TransferQuery.cpp
@@ -1,6 +1,6 @@
 /**
  * Transfers accelerator plugin for Orthanc
- * Copyright (C) 2018 Osimis, Belgium
+ * Copyright (C) 2018-2019 Osimis S.A., Belgium
  *
  * This program is free software: you can redistribute it and/or
  * modify it under the terms of the GNU Affero General Public License
